d8d6d20e08af9fc9be7f98fa2d8252b4b7c455fa
sam <me@sam.st>
2014-12-20 16:00:12 +0100

    Temporary logging.

--- radius.c
+++ radius.c
@@ -250,6 +250,8 @@ get_eap_user(void *c, const u8 *identity, size_t identity_len, int phase2,
 		return -1;
 	}
 
+	radius_msg_dump(request);
+
 	int res = -1;
 	struct sqlite_ctx *ctx = c;
 	sqlite3_reset(ctx->select_user);
