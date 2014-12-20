ed3ea3a663bc5cbd4a8ff06b029a57cae047bb2a
sam <me@sam.st>
2014-12-20 21:42:03 +0100

    Create tables if required.

--- radius.c
+++ radius.c
@@ -204,6 +204,45 @@ sqlite_init()
 		return NULL;
 	}
 
+	if (sqlite3_exec(ctx->db,
+
+        "CREATE TABLE IF NOT EXISTS users ("
+        "firstname TEXT NOT NULL,"
+        "lastname TEXT NOT NULL,"
+        "username TEXT PRIMARY KEY NOT NULL,"
+        "password TEXT NOT NULL,"
+        "expiration TEXT DEFAULT NULL"
+        ");"
+
+        "CREATE TABLE IF NOT EXISTS accounting ("
+        "timestamp TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+        "session TEXT NOT NULL, /* Acct-Session-Id (string) */"
+        "status INTEGER, /* Acct-Status-Type (int32) */"
+        "username TEXT, /* User-Name (string) */"
+        "ap_mac TEXT, /* Called-Station-Id (string) */"
+        "client_mac TEXT, /* Calling-Station-Id (string) */"
+        "session_time INTEGER, /* Acct-Session-Time (int32) */"
+        "input_octets INTEGER, /* Acct-Input-Octets (int32) */"
+        "output_octets INTEGER, /* Acct-Output-Octets (int32) */"
+        "terminate_cause INTEGER /* Acct-Terminate-Cause (int32) */"
+        ");"
+
+        "CREATE TABLE IF NOT EXISTS requests ("
+        "timestamp TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+        "session TEXT NOT NULL, /* Acct-Session-Id (string) */"
+        "status INTEGER, /* Radius Message Code */"
+        "username TEXT, /* User-Name (string) */"
+        "ap_mac TEXT, /* Called-Station-Id (string) */"
+        "client_mac TEXT /* Calling-Station-Id (string) */"
+        ");",
+
+			NULL, NULL, NULL) != SQLITE_OK) {
+
+		sqlite_deinit(&ctx);
+		wpa_printf(MSG_ERROR, "Failed to create tables");
+		return NULL;
+	}
+
 	if (sqlite3_prepare_v2(ctx->db,
 
 			"SELECT username, password FROM users "
